813b9c9ecc227ff3d98ea0e88ae4895159f99f78
Muhammad Taha <33485877+dydx-git@users.noreply.github.com>
2022-04-04 22:04:54 +0500

    end of task 1

--- task1.js
+++ task1.js
@@ -1,1 +1,14 @@
-// starting task 1
+const characterCounter = word => {
+    const result = [];
+    for (let index = 0; index < word.length; index++) {
+        const element = word[index];
+        result.push(`${element}-${index + 1}`);
+    }
+    return result.join(", ");
+}
+
+let htmlString = characterCounter("Sample");
+
+console.log(htmlString);
+
+document.getElementById("task1").innerHTML += htmlString;
